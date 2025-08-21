1ef3a0cb420418e1e5e1c5641569ea2816053311
RobHa93 <robin.hasler@bluewin.ch>
2025-08-21 13:09:27 +0200

    scrollToTop eingebaut

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
-import Anreise from './pages/Anreise';
 import UeberUns from './pages/UeberUns';
+import Anreise from './pages/Anreise';
 import Kontakt from './pages/Kontakt';
 import Sponsoren from './pages/Sponsoren';
 
-const App = () => {
+function App() {
   return (
     <Router>
-      <Navbar />
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/anreise" element={<Anreise />} />
         <Route path="/ueber-uns" element={<UeberUns />} />
+        <Route path="/anreise" element={<Anreise />} />
         <Route path="/kontakt" element={<Kontakt />} />
         <Route path="/sponsoren" element={<Sponsoren />} />
       </Routes>
     </Router>
   );
-};
+}
 
 export default App;
